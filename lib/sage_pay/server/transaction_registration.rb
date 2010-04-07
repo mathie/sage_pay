81e0a4ba6d1dec2e604a8c3c8b9916b66ad3f42f
Graeme Mathieson <mathie@woss.name>
2010-04-07 11:51:08 +0100

    More validations done.

--- lib/sage_pay/server/transaction_registration.rb
+++ lib/sage_pay/server/transaction_registration.rb
@@ -23,13 +23,30 @@ module SagePay
       validates_length_of :customer_email,   :maximum => 255
       validates_length_of :basket,           :maximum => 7_500
 
+      validates_inclusion_of :tx_type,           :allow_blank => true, :in => [ :payment, :deferred, :authenticate ]
+      validates_inclusion_of :allow_gift_aid,    :allow_blank => true, :in => [ true, false ]
+      validates_inclusion_of :apply_avs_cv2,     :allow_blank => true, :in => (0..3).to_a
+      validates_inclusion_of :apply_3d_secure,   :allow_blank => true, :in => (0..3).to_a
+      validates_inclusion_of :profile,           :allow_blank => true, :in => [:normal, :low]
+      validates_inclusion_of :billing_agreement, :allow_blank => true, :in => [true, false]
+      validates_inclusion_of :account_type,      :allow_blank => true, :in => [:ecommerce, :continuous_authority, :mail_order]
+
+      validates_true_for :amount, :key => :amount_minimum_value, :logic => lambda { amount.nil? || amount >= BigDecimal.new("0.01")   }, :message => "is less than the minimum value (0.01)"
+      validates_true_for :amount, :key => :amount_maximum_value, :logic => lambda { amount.nil? || amount <= BigDecimal.new("100000") }, :message => "is greater than the maximum value (100,000.00)"
+
       def initialize(attributes = {})
+        # Effectively hard coded for now
         @vps_protocol = "2.23"
 
         attributes.each do |k, v|
           send("#{k}=", v)
         end
       end
+
+      def amount=(value)
+        @amount = (value.nil? || (value.respond_to?(:empty?) && value.empty?)) ? nil : BigDecimal.new(value.to_s)
+      end
+
     end
   end
 end
--- spec/sage_pay/server/transaction_registration_spec.rb
+++ spec/sage_pay/server/transaction_registration_spec.rb
@@ -12,6 +12,26 @@ describe SagePay::Server::TransactionRegistration do
     transaction_registration.vps_protocol.should == "2.23"
   end
 
+  describe "conversions" do
+    it "should accept a float for the amount and convert it to a BigDecimal interally" do
+      transaction_registration = transaction_registration_factory(:amount => 12.34)
+      transaction_registration.should be_valid
+      transaction_registration.amount.should == BigDecimal.new("12.34")
+    end
+
+    it "should accept an integer for the amount and convert it to a BigDecimal interally" do
+      transaction_registration = transaction_registration_factory(:amount => 12)
+      transaction_registration.should be_valid
+      transaction_registration.amount.should == BigDecimal.new("12")
+    end
+
+    it "should accept a string for the amount and convert it to a BigDecimal interally" do
+      transaction_registration = transaction_registration_factory(:amount => "12")
+      transaction_registration.should be_valid
+      transaction_registration.amount.should == BigDecimal.new("12")
+    end
+  end
+
   describe "validations" do
     it { validates_the_presence_of(:transaction_registration, :tx_type)          }
     it { validates_the_presence_of(:transaction_registration, :vendor)           }
@@ -41,5 +61,132 @@ describe SagePay::Server::TransactionRegistration do
     it { validates_the_length_of(:transaction_registration, :customer_email,   :max => 255)   }
     it { validates_the_length_of(:transaction_registration, :basket,           :max => 7500)  }
 
+    it "should allow the amount to be a minimum of 0.01" do
+      transaction_registration = transaction_registration_factory(:amount => "0.01")
+      transaction_registration.should be_valid
+
+      transaction_registration = transaction_registration_factory(:amount => "0.00")
+      transaction_registration.should_not be_valid
+      transaction_registration.errors.on(:amount).should include("is less than the minimum value (0.01)")
+
+      transaction_registration = transaction_registration_factory(:amount => "-23")
+      transaction_registration.should_not be_valid
+      transaction_registration.errors.on(:amount).should include("is less than the minimum value (0.01)")
+    end
+
+    it "should allow the amount to be a maximum of 100,000.00" do
+      transaction_registration = transaction_registration_factory(:amount => "100000")
+      transaction_registration.should be_valid
+
+      transaction_registration = transaction_registration_factory(:amount => "100000.01")
+      transaction_registration.should_not be_valid
+      transaction_registration.errors.on(:amount).should include("is greater than the maximum value (100,000.00)")
+
+      transaction_registration = transaction_registration_factory(:amount => "123456")
+      transaction_registration.should_not be_valid
+      transaction_registration.errors.on(:amount).should include("is greater than the maximum value (100,000.00)")
+    end
+
+    it "should allow the transaction type to be one of :payment, :deferred or :authenticate" do
+      transaction_registration = transaction_registration_factory(:tx_type => :payment)
+      transaction_registration.should be_valid
+
+      transaction_registration = transaction_registration_factory(:tx_type => :deferred)
+      transaction_registration.should be_valid
+
+      transaction_registration = transaction_registration_factory(:tx_type => :authenticate)
+      transaction_registration.should be_valid
+
+      transaction_registration = transaction_registration_factory(:tx_type => :chickens)
+      transaction_registration.should_not be_valid
+      transaction_registration.errors.on(:tx_type).should include("is not in the list")
+    end
+
+    it "should allow the gift aid setting to be true or false" do
+      transaction_registration = transaction_registration_factory(:allow_gift_aid => true)
+      transaction_registration.should be_valid
+
+      transaction_registration = transaction_registration_factory(:allow_gift_aid => false)
+      transaction_registration.should be_valid
+
+      transaction_registration = transaction_registration_factory(:allow_gift_aid => "chickens")
+      transaction_registration.should_not be_valid
+      transaction_registration.errors.on(:allow_gift_aid).should include("is not in the list")
+    end
+
+    it "should allow apply_avs_cv2 to be 0 through 3 (see docs for what that means)" do
+      transaction_registration = transaction_registration_factory(:apply_avs_cv2 => 0)
+      transaction_registration.should be_valid
+
+      transaction_registration = transaction_registration_factory(:apply_avs_cv2 => 1)
+      transaction_registration.should be_valid
+
+      transaction_registration = transaction_registration_factory(:apply_avs_cv2 => 2)
+      transaction_registration.should be_valid
+
+      transaction_registration = transaction_registration_factory(:apply_avs_cv2 => 3)
+      transaction_registration.should be_valid
+
+      transaction_registration = transaction_registration_factory(:apply_avs_cv2 => 4)
+      transaction_registration.should_not be_valid
+      transaction_registration.errors.on(:apply_avs_cv2).should include("is not in the list")
+    end
+
+    it "should allow apply_3d_secure to be 0 through 3 (see docs for what that means)" do
+      transaction_registration = transaction_registration_factory(:apply_3d_secure => 0)
+      transaction_registration.should be_valid
+
+      transaction_registration = transaction_registration_factory(:apply_3d_secure => 1)
+      transaction_registration.should be_valid
+
+      transaction_registration = transaction_registration_factory(:apply_3d_secure => 2)
+      transaction_registration.should be_valid
+
+      transaction_registration = transaction_registration_factory(:apply_3d_secure => 3)
+      transaction_registration.should be_valid
+
+      transaction_registration = transaction_registration_factory(:apply_3d_secure => 4)
+      transaction_registration.should_not be_valid
+      transaction_registration.errors.on(:apply_3d_secure).should include("is not in the list")
+    end
+
+    it "should allow profile to be normal or low" do
+      transaction_registration = transaction_registration_factory(:profile => :normal)
+      transaction_registration.should be_valid
+
+      transaction_registration = transaction_registration_factory(:profile => :low)
+      transaction_registration.should be_valid
+
+      transaction_registration = transaction_registration_factory(:profile => :chickens)
+      transaction_registration.should_not be_valid
+      transaction_registration.errors.on(:profile).should include("is not in the list")
+    end
+
+    it "should allow billing_agreement to be true or false" do
+      transaction_registration = transaction_registration_factory(:billing_agreement => true)
+      transaction_registration.should be_valid
+
+      transaction_registration = transaction_registration_factory(:billing_agreement => false)
+      transaction_registration.should be_valid
+
+      transaction_registration = transaction_registration_factory(:billing_agreement => "chickens")
+      transaction_registration.should_not be_valid
+      transaction_registration.errors.on(:billing_agreement).should include("is not in the list")
+    end
+
+    it "should allow the account type to be one of ecommerce, continuous authority or mail order" do
+      transaction_registration = transaction_registration_factory(:account_type => :ecommerce)
+      transaction_registration.should be_valid
+
+      transaction_registration = transaction_registration_factory(:account_type => :continuous_authority)
+      transaction_registration.should be_valid
+
+      transaction_registration = transaction_registration_factory(:account_type => :mail_order)
+      transaction_registration.should be_valid
+
+      transaction_registration = transaction_registration_factory(:account_type => :chickens)
+      transaction_registration.should_not be_valid
+      transaction_registration.errors.on(:account_type).should include("is not in the list")
+    end
   end
 end
