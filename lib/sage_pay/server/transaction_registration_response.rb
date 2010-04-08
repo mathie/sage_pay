a3451dad483f42f2c937aa1094997b24305a691f
Graeme Mathieson <mathie@woss.name>
2010-04-08 12:49:55 +0100

    A transaction registration response object rubified.

--- lib/sage_pay/server/transaction_registration_response.rb
+++ lib/sage_pay/server/transaction_registration_response.rb
@@ -1,16 +1,93 @@
 module SagePay
   module Server
     class TransactionRegistrationResponse
-      include Validatable
+      attr_reader :vps_protocol, :status, :status_detail
+
+      def self.from_response_body(response_body)
+        key_converter = {
+          "VPSProtocol"  => :vps_protocol,
+          "Status"       => :status,
+          "StatusDetail" => :status_detail,
+          "VPSTxId"      => :transaction_id,
+          "SecurityKey"  => :security_key,
+          "NextURL"      => :next_url
+        }
+
+        value_converter = {
+          :status => {
+            "OK"        => :ok,
+            "MALFORMED" => :malformed,
+            "INVALID"   => :invalid,
+            "ERROR"     => :error
+          }
+        }
+
+        attributes = {}
+        response_body.each_line do |line|
+          key, value = line.split('=', 2)
+          unless key.nil? || value.nil?
+            value = value.chomp
+
+            converted_key = key_converter[key]
+            converted_value = value_converter[converted_key].nil? ? value : value_converter[converted_key][value]
+
+            attributes[converted_key] = converted_value
+          end
+        end
+
+        new(attributes)
+      end
 
       def initialize(attributes = {})
         attributes.each do |k, v|
-          send("#{k}=", v)
+          # We're only providing readers, not writers, so we have to directly
+          # set the instance variable.
+          instance_variable_set("@#{k}", v)
         end
       end
 
-      def self.from_response_body(response_body)
-        new
+      def ok?
+        status == :ok
+      end
+
+      def failed?
+        !ok?
+      end
+
+      def invalid?
+        status == :invalid
+      end
+
+      def malformed?
+        status == :invalid
+      end
+
+      def error?
+        status == :invalid
+      end
+
+      def transaction_id
+        if ok?
+          @transaction_id
+        else
+          raise RuntimeError, "Unable to retrieve the transaction id as the status was not OK."
+        end
+      end
+
+      def security_key
+        if ok?
+          @security_key
+        else
+          raise RuntimeError, "Unable to retrieve the security key as the status was not OK."
+        end
+      end
+
+      def next_url
+        if ok?
+          @next_url
+        else
+          raise RuntimeError, "Unable to retrieve the next URL as the status was not OK."
+        end
       end
     end
   end
--- spec/sage_pay/server/transaction_registration_response_spec.rb
+++ spec/sage_pay/server/transaction_registration_response_spec.rb
@@ -1,9 +1,121 @@
 require 'spec_helper'
 
-describe SagePay::Server::TransactionRegistrationResponse do
+include SagePay::Server
+
+describe TransactionRegistrationResponse do
   it "should work straight from the factory" do
     lambda {
-      transaction_registration_response_factory.should be_valid
+      transaction_registration_response_factory.should_not be_nil
     }.should_not raise_error
+  end
+
+  describe ".from_response_body" do
+    context "with an invalid response" do
+      before(:each) do
+        @response_body = <<-RESPONSE
+VPSProtocol=2.23
+Status=INVALID
+StatusDetail=4000 : The VendorName is invalid or the account is not active.
+        RESPONSE
+        @response = TransactionRegistrationResponse.from_response_body(@response_body)
+      end
+
+      it "should successfully parse the body" do
+        lambda {
+          TransactionRegistrationResponse.from_response_body(@response_body)
+        }.should_not raise_error
+      end
+
+      it "should report the vps_protocol as '2.23'" do
+        @response.vps_protocol.should == "2.23"
+      end
+
+      it "should report the status as :invalid" do
+        @response.status.should == :invalid
+      end
+
+      it "should report the status detail as the status detail message supplied" do
+        @response.status_detail.should == "4000 : The VendorName is invalid or the account is not active."
+      end
+
+      it "should admit to being invalid" do
+        @response.should be_invalid
+      end
+
+      it "should admit to having failed" do
+        @response.should be_failed
+      end
+
+      it "should raise an error if we try to ask for the transaction id" do
+        lambda {
+          @response.transaction_id
+        }.should raise_error RuntimeError, "Unable to retrieve the transaction id as the status was not OK."
+      end
+
+      it "should raise an error if we try to ask for the security key" do
+        lambda {
+          @response.security_key
+        }.should raise_error RuntimeError, "Unable to retrieve the security key as the status was not OK."
+      end
+
+      it "should raise an error if we try to ask for the next URL" do
+        lambda {
+          @response.next_url
+        }.should raise_error RuntimeError, "Unable to retrieve the next URL as the status was not OK."
+      end
+    end
+
+    context "with a valid response" do
+      before(:each) do
+        @response_body = <<-RESPONSE
+VPSProtocol=2.23
+Status=OK
+StatusDetail=Server transaction registered successfully.
+VPSTxId={728A5721-B45F-4570-937E-90A16B0A5000}
+SecurityKey=17F13DCBD8
+NextURL=https://test.sagepay.com/Simulator/VSPServerPaymentPage.asp?TransactionID={728A5721-B45F-4570-937E-90A16B0A5000}
+        RESPONSE
+
+        @response = TransactionRegistrationResponse.from_response_body(@response_body)
+      end
+
+      it "should successfully parse the body" do
+        lambda {
+          TransactionRegistrationResponse.from_response_body(@response_body)
+        }.should_not raise_error
+      end
+
+      it "should report the vps_protocol as '2.23'" do
+        @response.vps_protocol.should == "2.23"
+      end
+
+      it "should report the status as :OK" do
+        @response.status.should == :ok
+      end
+
+      it "should admit to being OK" do
+        @response.should be_ok
+      end
+
+      it "should not admit to being failed" do
+        @response.should_not be_failed
+      end
+
+      it "should report the status detail as the status detail message supplied" do
+        @response.status_detail.should == "Server transaction registered successfully."
+      end
+
+      it "should report the transaction id" do
+        @response.transaction_id.should == "{728A5721-B45F-4570-937E-90A16B0A5000}"
+      end
+
+      it "should report the security key" do
+        @response.security_key.should == "17F13DCBD8"
+      end
+
+      it "should report the next URL" do
+        @response.next_url.should == "https://test.sagepay.com/Simulator/VSPServerPaymentPage.asp?TransactionID={728A5721-B45F-4570-937E-90A16B0A5000}"
+      end
+    end
   end
 end
