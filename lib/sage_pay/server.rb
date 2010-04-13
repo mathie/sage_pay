92e231405e00479c5d3130be5c3a2b79bf87e362
Graeme Mathieson <mathie@woss.name>
2010-04-13 12:20:55 +0100

    Basic interface to the payment registration.

--- lib/sage_pay/server.rb
+++ lib/sage_pay/server.rb
@@ -1,5 +1,31 @@
 module SagePay
   module Server
-    # ...
+    class << self
+      attr_accessor :default_registration_options
+    end
+    # NOTE: Expected to be something along the lines of:
+    #
+    #     SagePay::Server.default_registration_options = {
+    #       :mode => :live,
+    #       :vendor => "rubaidh",
+    #       :notification_url => "http://test.host/notification"
+    #     }
+    #
+    # ... which you'll want to stick in config/initializers/sage_pay.rb or one
+    # of your environment files, if you're doing this with a Rails app.
+    self.default_registration_options = {}
+
+    def self.payment(attributes = {})
+      registration({ :tx_type => :payment }.merge(attributes))
+    end
+
+    def self.registration(attributes)
+      defaults = {
+        :vendor_tx_code   => TransactionCode.random,
+        :delivery_address => attributes[:billing_address]
+      }.merge(default_registration_options)
+
+      SagePay::Server::TransactionRegistration.new(defaults.merge(attributes))
+    end
   end
 end
---
+++ spec/sage_pay/server_spec.rb
@@ -0,0 +1,72 @@
+require 'spec_helper'
+
+describe SagePay::Server do
+  describe ".payment" do
+    before(:each) do
+      SagePay::Server.default_registration_options = {
+        :mode => :test,
+        :vendor => "rubaidh",
+        :notification_url => "http://test.host/notification"
+      }
+    end
+
+    after(:each) do
+      SagePay::Server.default_registration_options = {}
+    end
+
+    it "should pass in the default registration options" do
+
+      payment = SagePay::Server.payment
+      payment.mode.should             == :test
+      payment.vendor.should           == "rubaidh"
+      payment.notification_url.should == "http://test.host/notification"
+    end
+
+    it "should generate a vendor transaction code automatically" do
+      payment = SagePay::Server.payment
+      payment.vendor_tx_code.should_not be_nil
+    end
+
+    it "should set the transaction type to :payment" do
+      payment = SagePay::Server.payment
+      payment.tx_type.should == :payment
+    end
+
+    it "should duplicate the billing address to the delivery address if no billing address is supplied" do
+      address = mock("Home address")
+
+      payment = SagePay::Server.payment :billing_address => address
+      payment.delivery_address.should == address
+    end
+
+    it "should not overwrite a delivery address if one is supplied" do
+      billing_address = mock("Billing address")
+      delivery_address = mock("Delivery address")
+
+      payment = SagePay::Server.payment :billing_address => billing_address, :delivery_address => delivery_address
+      payment.delivery_address.should == delivery_address
+      payment.billing_address.should  == billing_address
+    end
+
+    it "should allow the caller to override any of the default registration options" do
+      payment = SagePay::Server.payment :vendor => "chickens"
+      payment.vendor.should == "chickens"
+    end
+
+    it "should allow the caller to override any of the calculated default registration options (eg vendor tx code)" do
+      payment = SagePay::Server.payment :vendor_tx_code => "chickens"
+      payment.vendor_tx_code.should == "chickens"
+    end
+
+    it "should generate a valid payment given the minimum set of fields" do
+      address = address_factory
+      payment = SagePay::Server.payment(
+        :description => "Demo payment",
+        :amount => 12.34,
+        :currency => "GBP",
+        :billing_address => address
+      )
+      payment.should be_valid
+    end
+  end
+end
