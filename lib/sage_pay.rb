9fc00926e8ced50530e80f984734b730a649af6b
Graeme Mathieson <mathie@woss.name>
2010-04-14 18:10:03 +0100

    SagePay seems to think that curly braces are OK in query strings. Ruby's URI library thinks not.

--- lib/sage_pay.rb
+++ lib/sage_pay.rb
@@ -11,6 +11,7 @@ end
 
 require 'validatable-ext'
 
+require 'sage_pay/uri_fixups'
 require 'sage_pay/server/address'
 require 'sage_pay/server/transaction_code'
 require 'sage_pay/server/signature_verification_details'
