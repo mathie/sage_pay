bbbdb02bb4204fbb29b8f5bd9fc4ad67d60cb667
Miguel Diaz <miguel.diaz@codescrum.com>
2012-04-02 00:46:59 -0500

    added readers for token and expiry date in notification

--- lib/sage_pay/server/notification.rb
+++ lib/sage_pay/server/notification.rb
@@ -5,7 +5,7 @@ module SagePay
                   :status, :status_detail, :tx_auth_no, :avs_cv2, :address_result,
                   :post_code_result, :cv2_result, :gift_aid, :threed_secure_status,
                   :cavv, :address_status, :payer_status,:card_type, :last_4_digits,
-                  :vps_signature
+                  :vps_signature, :token, :expiry_date
 
       def self.from_params(params, signature_verification_details = nil)
         key_converter = {
