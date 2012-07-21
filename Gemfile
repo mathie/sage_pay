a75558bf26b77de723332e928aab04c20397cf59
Graeme Mathieson <mathie@woss.name>
2012-07-21 16:02:51 +0100

    Rcov-compatible coverage reports for Jenkins.

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 # Source gems from the default public repository
 source :rubygems
 
-gem 'simplecov'
+gem 'simplecov', :require => false
+gem 'simplecov-rcov', :require => false
 
 gemspec
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,15 @@ require 'rubygems'
 
 if ENV['COVERAGE']
   require 'simplecov'
+  require 'simplecov-rcov'
+  class SimpleCov::Formatter::MergedFormatter
+    def format(result)
+      SimpleCov::Formatter::HTMLFormatter.new.format(result)
+      SimpleCov::Formatter::RcovFormatter.new.format(result)
+    end
+  end
+  SimpleCov.formatter = SimpleCov::Formatter::MergedFormatter
+
   SimpleCov.start do
     add_filter '/spec/'
   end
