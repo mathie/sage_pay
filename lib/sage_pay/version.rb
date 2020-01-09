e4f93b057528d663c80c05f417eedd3e7da3c342
Peter Vandenberk <peter.vandenberk@simplybusiness.co.uk>
2020-01-09 13:39:33 +0000

    bump version for new gemstash release

--- lib/sage_pay/version.rb
+++ lib/sage_pay/version.rb
@@ -1,5 +1,5 @@
 module SagePay
-  base = '1.0.2'
+  base = '1.0.3'
 
   # SB-specific versioning "algorithm" to accommodate BNW/Jenkins/gemstash
   VERSION = (pre = ENV.fetch('GEM_PRE_RELEASE', '')).empty? ? base : "#{base}.#{pre}"
